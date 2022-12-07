29dca61e84f46e2867868869dcefc1df96d0390d
Shadi Nakhla <shadinakhla13@gmail.com>
2022-12-07 18:37:48 +0200

    added contact creation

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,24 @@ import { useState } from "react";
 import * as ContactsAPI from "../utils/ContactsAPI";
 import { useEffect } from "react";
 import CreateContact from "./CreateContact";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 const App = () => {
+  let navigate = useNavigate();
   const [contacts, setContacts] = useState([]);
   const removeContact = (contact) => {
     setContacts(contacts.filter((c) => c.id !== contact.id));
   };
+  const createContact = (contact) => {
+    const create = async () => {
+      const res = await ContactsAPI.create(contact);
+      setContacts(contacts.concat(res));
+    };
+
+    create();
+    navigate("/");
+  };
+  // setContacts(...contacts,res)
   useEffect(() => {
     const getContacts = async () => {
       const res = await ContactsAPI.getAll();
@@ -28,7 +39,17 @@ const App = () => {
         }
       />
 
-      <Route exact path="/create" element={<CreateContact />} />
+      <Route
+        exact
+        path="/create"
+        element={
+          <CreateContact
+            onCreateContact={(contact) => {
+              createContact(contact);
+            }}
+          />
+        }
+      />
     </Routes>
   );
 };
--- src/components/CreateContact.js
+++ src/components/CreateContact.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput';
+import serializeForm from "form-serialize"
 
-export default function CreateContact() {
+
+export default function CreateContact({onCreateContact}) {
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const values = serializeForm(e.target,{hash: true})
+
+        if (onCreateContact){
+            onCreateContact(values);
+        }
+        console.log(values)
+    }
   return (
-    <div>CreateContact</div>
+    <div>
+    <Link to='/' className='close-create-contact'>Close</Link>
+    <form onSubmit={handleSubmit} className='create-contact-form'>
+    <ImageInput className='create-contact-avatar-input' name='avatarURL'/>
+    <div className='create-contact-details'>
+    <input name='name' placeholder='Name' type='text'/>
+    <input name='handle' placeholder='Handle' type='text'/>
+    <button type='submit'>Add Contact</button>
+    </div>
+    </form>
+    </div>
   )
 }
